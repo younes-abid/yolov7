d9b1f970e6f1d6dae871e1a90e921ff9377bdc5d
younes <mr.younes.abid@gmail.com>
2022-08-15 14:01:59 +0400

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ import torch
 import yaml
 from tqdm import tqdm
 
-from .models.experimental import attempt_load
+from models.experimental import attempt_load
 from .utils.datasets import create_dataloader
 from .utils.general import coco80_to_coco91_class, check_dataset, check_file, check_img_size, check_requirements, \
     box_iou, non_max_suppression, scale_coords, xyxy2xywh, xywh2xyxy, set_logging, increment_path, colorstr
